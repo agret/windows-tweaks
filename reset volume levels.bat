fd6027fc397f9af2a494533b9135dcf4e0e2f6e8
Andrew Fox <alias.zero2097@gmail.com>
2019-08-01 14:57:40 +1000

    Add files via upload

---
+++ reset volume levels.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+ECHO Reset Volume Mixer Settings...
+
+NET STOP "Creative Audio Service"
+NET STOP Audiosrv
+NET STOP AudioEndpointBuilder
+
+REG DELETE "HKCU\Software\Microsoft\Internet Explorer\LowRegistry\Audio\PolicyConfig\PropertyStore" /F
+REG ADD "HKCU\Software\Microsoft\Internet Explorer\LowRegistry\Audio\PolicyConfig\PropertyStore"
+
+NET START Audiosrv
+NET START "Creative Audio Service"
---
+++ restart_nvidia_driver.bat
@@ -0,0 +1,2 @@
+pushd "%~dp0"
+devmanview.exe /disable_enable "NVIDIA GeForce GTX 1080"
